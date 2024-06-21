b4a48f88808167dda63b3fe093347e91ccfa5673
aplata <and.202.ag@gmail.com>
2024-06-20 20:08:06 -0600

    modifed image

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 
 setup (
     name='em_simulation',
-    version='0.5',
+    version='0.6',
     license='MIT',
     description='2D Electromagnetic Simulator for GPR Scenarios',
-    long_description=open('README.md',encoding='utf-8').read(),
+    long_description=long_description,
     long_description_content_type='text/markdown',
     author='Aplatag',
     packages=find_packages(),
